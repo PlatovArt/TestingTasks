4d6359da2abd86b92e6ec056f039801254f94dc3
DarkCrusade <DarkCrusade@DESKTOP-PHKBRC4>
2022-02-07 11:09:51 +0300

    Добавьте файлы проекта.

---
+++ TaskProcessKiller/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskProcessKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string processName = args[0];
+            int processLifeTime = Convert.ToInt32(args[1]) * 60000;
+            int checkTime = Convert.ToInt32(args[2]) * 60000;
+            MonitorKeypressAsync();          
+            while (true)
+            {
+                DateTime date = DateTime.Now;
+                Process[] process = Process.GetProcessesByName(processName);
+                foreach (Process check in process)
+                {
+                    if ((date - check.StartTime).TotalMilliseconds > processLifeTime)
+                    {
+                        check.Kill();
+                        Console.WriteLine("Процесс " + processName + " был закрыт");
+                    }
+                }
+                Thread.Sleep(checkTime);               
+            }
+        }
+        public static async void MonitorKeypressAsync()
+        {
+            await Task.Run(() => MonitorKeypress());
+            
+        }
+
+        static void MonitorKeypress()
+        {
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            do
+            {
+                cki = Console.ReadKey();
+            } while (cki.Key != ConsoleKey.Q);
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ TestingTaskSelenium/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace TestingTaskSelenium
+{
+    internal class Program
+    {
+        public static string url = "https://careers.veeam.ru/vacancies";
+        public static IWebElement webElement;
+        public static ChromeDriver driver;
+        public const string departmentField = "//*[contains(text(),'Все отделы')]";
+        public const string languageField = "//*[contains(text(),'Все языки')]";
+        public static int numberOfRealSample;
+
+        static void Main(string[] args)
+        {
+            StartAndPrepareChrome();
+
+            string departmentFilter = args[0];
+            departmentFilter = departmentFilter.Replace('_', ' ');
+            string languageFilter = args[1];
+            int expectedSample = Convert.ToInt32(args[2]);
+
+            FindAndClick(departmentField);
+            FindAndClick("//*[contains(text(),'" + departmentFilter + "')]");
+            FindAndClick(languageField);
+            FindAndClick("//*[contains(text(),'" + languageFilter + "')]");
+            var list = driver.FindElements(By.ClassName("card"));
+            numberOfRealSample = list.Count;
+            if (numberOfRealSample != expectedSample)
+                Console.WriteLine("Размеры ожидаемой и действительной выборки не совпадают");
+        }
+
+        public static void StartAndPrepareChrome()
+        {
+            driver = new ChromeDriver();
+            driver.Url = url;
+            driver.Manage().Window.Maximize();
+        }
+        public static void FindAndClick(string xPath)
+        {
+            webElement = driver.FindElement(By.XPath(xPath));
+            webElement.Click();
+        }
+    }
+}
